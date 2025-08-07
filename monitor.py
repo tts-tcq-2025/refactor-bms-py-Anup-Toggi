bc7586430ff44ab60020db46fd06130a0cbdc797
Anup Toggi <89697446+Anup-Toggi@users.noreply.github.com>
2025-08-07 12:11:53 +0530

    Refactor monitor.py

--- monitor.py
+++ monitor.py
@@ -1,37 +1,36 @@
-
 from time import sleep
 import sys
 
+def is_temperature_ok(temperature):
+    return 95 <= temperature <= 102
 
-def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  return True
+def is_pulse_rate_ok(pulse_rate):
+    return 60 <= pulse_rate <= 100
+
+def is_spo2_ok(spo2):
+    return spo2 >= 90
+
+def vitals_status(temperature, pulse_rate, spo2):
+    if not is_temperature_ok(temperature):
+        return False, 'Temperature critical!'
+    if not is_pulse_rate_ok(pulse_rate):
+        return False, 'Pulse Rate is out of range!'
+    if not is_spo2_ok(spo2):
+        return False, 'Oxygen Saturation out of range!'
+    return True, 'All vitals normal.'
+
+def blink_warning(times=6, delay=1):
+    for _ in range(times):
+        print('\r* ', end='')
+        sys.stdout.flush()
+        sleep(delay)
+        print('\r *', end='')
+        sys.stdout.flush()
+        sleep(delay)
+
+def vitals_ok(temperature, pulse_rate, spo2):
+    ok, message = vitals_status(temperature, pulse_rate, spo2)
+    if not ok:
+        print(message)
+        blink_warning()
+    return ok
